c529ae408383b6a3534e67d046c4973b72556f30
bayukrist <672018005@student.uksw.edu>
2022-04-08 20:25:42 +0700

    membuat ulang tampilan

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
 $(document).ready(function () {
-    $("#navHome").click(function() { 
-        $("#contentPortfolio").hide();
-        $("#contentHome").fadeIn(1500);
+    // $("#navHome").click(function() { 
+    //     $("#contentPortfolio").hide();
+    //     $("#contentHome").fadeIn(1500);
         
-    });
+    // });
 
-    $("#navPorto").click(function() { 
-        $("#contentHome").hide();
-        $("#contentPortfolio").fadeIn(1500);
+    // $("#navPorto").click(function() { 
+    //     $("#contentHome").hide();
+    //     $("#contentPortfolio").fadeIn(1500);
        
-    });
+    // });
 
-    $('.nav-link').click(function () {
-        $('.nav-link').css('color', '#3F3351');
-        $(this).css('color', 'gray');
-    });
+    // $('.nav-link').click(function () {
+    //     $('.nav-link').css('color', '#3F3351');
+    //     $(this).css('color', 'gray');
+    // });
 
+});
+
+$('.first li').click(function(){
+    $(this).toggleClass("shadow-1").siblings();
+    $(this).toggleClass("fill-color").siblings();
+  });
+
+  var typed = new Typed('.animate', {
+    strings: [
+      'simple',
+      'impactful'
+    ],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true
+  });
+
+  $(window).on('load', function(){
+    $('.HeaderContent').addClass('muncul');
+    $('.catalog').each(function (i) { 
+        setTimeout(function(){
+           $('.catalog').eq(i).addClass('muncul');
+        }, 300 * (i+1));
+   });
 });
